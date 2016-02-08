bde46d28746d8aba8a08499b6a884dfb57787bf8
Cam Soper <cam.soper@outlook.com>
2016-02-08 12:07:08 -0600

    Got rid of the left sidebar and just moved the menu button to the titlebar.

--- WhosDown/MainPage.xaml.cs
+++ WhosDown/MainPage.xaml.cs
@@ -20,6 +20,8 @@ using Windows.UI.ViewManagement;
 using Windows.UI;
 using Windows.System;
 using Windows.UI.Popups;
+using Windows.ApplicationModel.Core;
+using Windows.UI.Core;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -36,43 +38,48 @@ namespace ChatsApp
         {
             this.InitializeComponent();
             MyWebview.RegisterPropertyChangedCallback(WebView.DocumentTitleProperty, OnDocTitleChanged);
-
         }
 
         private async void Page_Loaded(object sender, RoutedEventArgs e)
         {
-            
-
+            // Set up interface
             AboutText.Text = this.ApplicationVersion;
             NotificationToggle.IsOn = this.EnableNotifications;
-            if(Windows.Storage.ApplicationData.Current.LocalSettings.Values["AdvancedMode"] != null)
-            {
-                MySplitView.DisplayMode = SplitViewDisplayMode.Overlay;
-            }
+            DisplayTitleBar(CoreApplication.GetCurrentView().TitleBar, null);
+            Window.Current.SetTitleBar(MainTitleBar);
 
+            // Bind Events
+            Window.Current.Activated += Current_Activated;
+            CoreApplication.GetCurrentView().TitleBar.IsVisibleChanged += DisplayTitleBar;
+            CoreApplication.GetCurrentView().TitleBar.LayoutMetricsChanged += DisplayTitleBar;
+
+            // Make WhatsApp request
             HttpRequestMessage req = new HttpRequestMessage(HttpMethod.Get, new Uri("https://web.whatsapp.com"));
             req.Headers.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2486.0 Safari/537.36");
             this.MyWebview.NavigateWithHttpRequestMessage(req);
 
-            var mobileWarned = Windows.Storage.ApplicationData.Current.LocalSettings.Values["MobileWarned"];
+            // Display warning for Continuum
+            await ShowContinuumWarning();
 
+        }
+
+        private static async System.Threading.Tasks.Task ShowContinuumWarning()
+        {
+            var mobileWarned = Windows.Storage.ApplicationData.Current.LocalSettings.Values["MobileWarned"];
             if (App.IsMobile && mobileWarned == null)
             {
                 var message = "Hey, it looks like you're running this on Windows 10 Mobile! That's cool, just be aware this is meant for running on a " +
                                 "second monitor with Continuum. If you don't have a second screen (or a second device with the official WhatsApp app), you're not going to " +
-                                "be able to scan this QR code." + 
+                                "be able to scan this QR code." +
                                 "\r\n\r\n" +
                                 "I won't bug you about this again.";
                 var dialog = new MessageDialog(message);
                 dialog.Commands.Add(new UICommand("Got it!"));
                 dialog.CancelCommandIndex = 0;
                 await dialog.ShowAsync();
-
                 Windows.Storage.ApplicationData.Current.LocalSettings.Values["MobileWarned"] = true;
             }
-
         }
-
 
         private void MyWebview_PermissionRequested(WebView sender, WebViewPermissionRequestedEventArgs args)
         {
@@ -120,30 +127,20 @@ namespace ChatsApp
 
         public static int ConvertToInt(String input)
         {
-            // Replace everything that is no a digit.
+            // Strip the number from a string
             String inputCleaned = Regex.Replace(input, "[^0-9]", "");
-
             int value = 0;
-
-            // Tries to parse the int, returns false on failure.
             if (int.TryParse(inputCleaned, out value))
             {
-                // The result from parsing can be safely returned.
                 return value;
             }
 
-            return 0; // Or any other default value.
-        }
-
-        private void HamburgerButton_Click(object sender, RoutedEventArgs e)
-        {
-            MySplitView.IsPaneOpen = !MySplitView.IsPaneOpen;
+            return 0; 
         }
 
         private void SettingsButton_Click(object sender, RoutedEventArgs e)
         {
             ContentSplitView.IsPaneOpen = !ContentSplitView.IsPaneOpen;
-            MySplitView.IsPaneOpen = false;
         }
 
         bool DisplayNotifications
@@ -202,24 +199,26 @@ namespace ChatsApp
             this.EnableNotifications = NotificationToggle.IsOn;
         }
 
-        private async void AdvancedMode_Click(object sender, RoutedEventArgs e)
+        private void Current_Activated(object sender, WindowActivatedEventArgs e)
         {
-            var warning = "This option will permanently remove the left side menu.  You won't be able to regain access to the menu or this settings pane until you re-install WhosDown." +
-                            "\r\n\r\n" +
-                            "Are you sure?";
-            var message = new MessageDialog(warning, "Warning");
-            message.Commands.Add(new UICommand("OK", new UICommandInvokedHandler(HandleAdvancedModeDialog)));
-            message.Commands.Add(new UICommand("Cancel"));
-            message.DefaultCommandIndex = 1;
-            message.CancelCommandIndex = 1;
-            await message.ShowAsync();
+            if (e.WindowActivationState != CoreWindowActivationState.Deactivated)
+            {
+                TitleBar.Background = new SolidColorBrush(Color.FromArgb(255, 0, 150, 136));
+                TitleText.Foreground = new SolidColorBrush(Color.FromArgb(255, 255, 255, 255));
+            }
+            else
+            {
+                TitleBar.Background = new SolidColorBrush(Color.FromArgb(255, 255, 255, 255));
+                TitleText.Foreground = new SolidColorBrush(Color.FromArgb(128, 0, 0, 0));
+            }
+
+            SettingsButton.Foreground = TitleText.Foreground;
         }
 
-        private void HandleAdvancedModeDialog(IUICommand command)
+        private void DisplayTitleBar(CoreApplicationViewTitleBar coreTitleBar, object args)
         {
-            ContentSplitView.IsPaneOpen = false;
-            MySplitView.DisplayMode = SplitViewDisplayMode.Overlay;
-            Windows.Storage.ApplicationData.Current.LocalSettings.Values["AdvancedMode"] = true;
+            coreTitleBar.ExtendViewIntoTitleBar = coreTitleBar.IsVisible;
+            if (coreTitleBar.IsVisible) { TitleBar.Height = coreTitleBar.Height; }
         }
     }
 }
